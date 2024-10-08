e3147820ea47be718126b719aeeed45f266e0b7b
Felix <reba.felix@web.de>
2024-10-08 14:24:40 +0200

    Factory zum Erzeugen des Translators

---
+++ src/org/hbrs/se1/ws24/exercises/uebung1/control/GermanTranslatorFactory.java
@@ -0,0 +1,7 @@
+package org.hbrs.se1.ws24.exercises.uebung1.control;
+
+public class GermanTranslatorFactory {
+    public static GermanTranslator createGermanTranslator() {
+        return new GermanTranslator();
+    }
+}
