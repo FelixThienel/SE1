a14453fcb9f7d9d2039709afc3788c134b1bf60c
Felix <reba.felix@web.de>
2024-10-08 13:51:32 +0200

    Interface

--- src/org/hbrs/se1/ws24/exercises/uebung1/control/Translator.java
+++ src/org/hbrs/se1/ws24/exercises/uebung1/control/Translator.java
@@ -13,7 +13,7 @@ interface Translator {
 	
 	double version = 1.0; // Version des Interface
 	
-	/*
+	/**
 	 * Uebersetzt eine numerische Zahl in eine String-basierte
 	 * Repraesentation gemaess der Spezifikation in der Aufgabe 1-2 
 	 */
