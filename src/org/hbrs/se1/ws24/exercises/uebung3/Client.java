99943e851dbfc842eb13a70fc9dd7e6a81cd8c6a
Felix <reba.felix@web.de>
2024-10-22 23:05:08 +0200

    Client Klasse zum Testen der Ausgabe auf der Konsole

---
+++ src/org/hbrs/se1/ws24/exercises/uebung3/Client.java
@@ -0,0 +1,20 @@
+package org.hbrs.se1.ws24.exercises.uebung3;
+
+import org.hbrs.se1.ws24.exercises.uebung3.persistence.PersistenceException;
+
+public class Client {
+    public void test(Container container) {
+        MemberView view = new MemberView();
+        Member m1 = new ConcreteMember(1);
+        Member m2 = new ConcreteMember(2);
+        Member m3 = new ConcreteMember(3);
+        try {
+            container.addMember(m1);
+            container.addMember(m2);
+            container.addMember(m3);
+            view.dump(container.getCurrentList());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
