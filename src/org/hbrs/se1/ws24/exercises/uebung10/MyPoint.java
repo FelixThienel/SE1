46fff99daa2f9414838571acb440ba783aee2c9f
Felix <reba.felix@web.de>
2025-01-14 23:17:12 +0100

    MyPoint

---
+++ src/org/hbrs/se1/ws24/exercises/uebung10/MyPoint.java
@@ -0,0 +1,40 @@
+package org.hbrs.se1.ws24.exercises.uebung10;
+
+public class MyPoint {
+    private double x;
+    private double y;
+
+    public MyPoint(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        MyPoint other = (MyPoint) obj;
+        if (Double.doubleToLongBits(x) != Double.doubleToLongBits(other.x)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(y) != Double.doubleToLongBits(other.y)) {
+            return false;
+        }
+        return true;
+    }
+}
